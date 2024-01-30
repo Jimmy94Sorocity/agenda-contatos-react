e43729248f3df18a1894019d3a8c68edf9fd9047
Matheus Fré <matheusfre94@gmail.com>
2024-01-29 21:37:49 -0300

    organizando a pasta de styles principal

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
 import styled from "styled-components";
+import variaveis from "../../styles/variaveis";
 
 export const BotaoAdicionar = styled(Link)`
   padding: 8px;
   border: 2px solid green;
   border-radius: 8px;
-  background-color: #fff;
+  background-color: ${variaveis.verde};
+  color: #fff;
   margin-bottom: 16px;
   text-decoration: none;
 `;
